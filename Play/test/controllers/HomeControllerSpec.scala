d167514587eb234a72e1cf14666786d6402f3673
Gnanamanickam A <gnanamanickam1995@gmail.com>
2021-11-05 17:13:38 -0500

    Play test cases

--- Play/test/controllers/HomeControllerSpec.scala
+++ Play/test/controllers/HomeControllerSpec.scala
@@ -5,7 +5,12 @@ import org.scalatestplus.play._
 import org.scalatestplus.play.guice._
 import play.api.test._
 import play.api.test.Helpers._
+import org.junit.runner._
+import org.scalatestplus.junit.JUnitRunner
+import play.api.test._
+import play.test.WithBrowser
 
+@RunWith(classOf[JUnitRunner])
 class restClientControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting {
 
   "HomeController GET" should {
@@ -29,5 +34,14 @@ class restClientControllerSpec extends PlaySpec with GuiceOneAppPerTest with Inj
       val home = route(app, request).get
       status(home) mustBe NOT_FOUND
     }
+
+//    "Application" should {
+//
+//      "Work from a browser" in new WithBrowser {
+//
+//        browser.goTo("http://localhost:9000/logAPI")
+//        browser.pageSource must contain("\"{\\\"isPresent\\\": \\\"True\\\"}\"")
+//      }
+//    }
   }
 }
